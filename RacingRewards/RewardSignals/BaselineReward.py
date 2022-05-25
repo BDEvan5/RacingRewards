50c90b061562ed9089404a247e60e347fe476acf
Benjamin <benjaminevans316@gmail.com>
2022-05-25 12:53:34 +0200

    Clean up

--- RacingRewards/RewardSignals/BaselineReward.py
+++
@@ -1,8 +0,0 @@
-
-
-class BaselineReward:
-    def __init__(self, run) -> None:
-        self.r_bias = run.r_bias
-
-    def __call__(self, state, s_prime):
-        return self.r_bias + s_prime['reward']
