d9419c47948f45855a43bd48216dc83fe8f3dc81
Benjamin <benjaminevans316@gmail.com>
2022-07-21 12:08:12 +0200

    Config for results training graph in Safety colab paper

---
+++ RacingRewards/DataTools/RewardPlotter.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tikzplotlib
+import csv
+
+def moving_average(data, period):
+    ret = np.convolve(data, np.ones(period), 'same') / period
+    for i in range(period):
+        # t = np.mean
+        t = np.convolve(data, np.ones(i+1), 'valid') / (i+1)
+        ret[i] = t[0]
+        ret[-i-1] = t[-1]
+    return ret
+
+def load_csv_reward():
+    rewards, lengths = [], []
+    path = "Data/Vehicles/BaselineSSS/BaselineSSS_levine_blocked_1_0_0/"
+    with open(f"{path}training_data.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if float(row[2]) > 0:
+                rewards.append(float(row[1]))
+                lengths.append(float(row[2]))
+    return rewards, lengths
+
+from matplotlib.ticker import MultipleLocator
+def plot_reward_steps():
+    path = "Data/Vehicles/BaselineSSS/BaselineSSS_levine_blocked_1_0_0/"
+    rewards, lengths = load_csv_reward()
+
+    plt.figure(1)
+    steps = np.cumsum(lengths) / 100
+    plt.plot(steps, rewards, '.', color='darkblue', markersize=4)
+    plt.plot(steps, moving_average(rewards, 20), linewidth='4', color='r')
+    plt.gca().get_yaxis().set_major_locator(MultipleLocator(1))
+
+    plt.xlabel("Training Steps (x100)")
+    plt.ylabel("Reward per Episode")
+
+    plt.tight_layout()
+    plt.grid()
+
+    tikzplotlib.save(path + "baseline_reward_plot.tex", strict=True, extra_axis_parameters=['height=4cm', 'width=0.5\\textwidth', 'clip mode=individual'])
+
+    plt.show()
+
+
+    plt.show()
+
+plot_reward_steps()
